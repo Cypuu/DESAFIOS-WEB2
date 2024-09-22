feeaa1f87925a5e413dbc3a6bcf092df68a181c3
Cypuu <jcypuzak@gmail.com>
2024-09-22 14:59:54 -0300

    Desafío 2, realizado en index.js

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+let NombreProducto = "PS5"
+let PrecioUnitario = "1100000"
+
+let CantidadDeseada = prompt("¿Qué cantidad desea comprar?: ")
+
+const CostoTotal = PrecioUnitario * CantidadDeseada
+
+alert("Costo total de la compra: " + CostoTotal)
